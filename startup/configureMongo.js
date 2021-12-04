2c88269527ce3b39251405df78fabdb6d8a8513d
andriipetrashchuk <triumph.town99@gmail.com>
2021-12-04 19:16:14 +0200

    added mongourl to env

--- startup/configureMongo.js
+++ startup/configureMongo.js
@@ -4,7 +4,7 @@ const config = require('config');
 
 
 const configureMongo = () => {
-    const mongoUrl = process.env.URL || config.get('url');
+    const mongoUrl = process.env.MONGODB_URI
 
     mongoose.connect(mongoUrl, {
             useNewUrlParser: true,
