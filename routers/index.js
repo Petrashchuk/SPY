bb7516500511e2439821b611d48b8b80b452298e
andriipetrashchuk <triumph.town99@gmail.com>
2021-12-04 19:30:20 +0200

    test

--- routers/index.js
+++ routers/index.js
@@ -8,8 +8,8 @@ router.get('*', (req,res)=>{
 }).post('/',async (req,res) => {
 
     try {
-        const response = await UserModel.create(req.body);
-        res.status(200).json(response);
+
+        res.status(200).json({ok:'ok'});
     }
     catch (e) {
         console.error(e.message);
